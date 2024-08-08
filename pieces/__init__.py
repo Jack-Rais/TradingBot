f66b71e70548a1acc5b8541be20a7ee0d5a39064
Jack-Rais <107562993+Jack-Rais@users.noreply.github.com>
2024-08-08 18:44:18 +0200

    Update __init__.py

--- pieces/__init__.py
+++ pieces/__init__.py
@@ -1,3 +1,15 @@
 try:
 
-  from Driver import TradingDriver
+  from Environment import TradingEnv
+  from GymEnvironment import TradingEnv
+  from Metric import TradingMetric
+  from Observation import Observer
+  from Net import TradingNet
+
+except Exception:
+
+  from pieces.Environment import TradingEnv
+  from pieces.GymEnvironment import TradingEnv
+  from pieces.Metric import TradingMetric
+  from pieces.Observation import Observer
+  from pieces.Net import TradingNet
